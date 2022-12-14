e2fbf3cdd91446e46a4a8091e2a18b96c93dcfb0
Fionna Chan <fionnacst@gmail.com>
2022-12-14 22:21:19 +0000

    first draft

---
+++ src/components/HorizontalRuleWithText/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+export default function HorizontalRuleWithText({ children }) {
+  return (
+    <div className={styles["horizontal-rule"]}>
+      <span>{children}</span>
+    </div>
+  );
+}
