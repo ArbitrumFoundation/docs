338eef4ba0cc3f2c740f1fac33d373a055e8aa43
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2022-12-16 17:37:13 +0100

    rename index to example

--- src/pages/example.tsx
+++ src/pages/example.tsx
@@ -5,7 +5,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
-import styles from './index.module.css';
+import styles from './example.module.css';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
