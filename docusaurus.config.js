011adce45e51154f36b8563b19ad389010caa619
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2022-12-16 17:41:00 +0100

    clean up

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,7 +75,7 @@ const config = {
             items: [
               {
                 label: 'Get started',
-                to: 'a-gentle-introduction',
+                to: '/',
               },
             ],
           },
--- src/pages/example.tsx
+++ src/pages/example.tsx
@@ -15,10 +15,7 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/a-gentle-introduction"
-          >
+          <Link className="button button--secondary button--lg" to="/">
             A Gentle Introduction to Arbitrum DAO
           </Link>
         </div>
