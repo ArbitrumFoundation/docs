09616160ae1fa7001953a86d43fd4449c0b0fd3c
Fionna Chan <fionnacst@gmail.com>
2023-01-17 11:23:18 +0000

    re-render tippys on page change

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Footer from '@theme-original/Footer';
 import GlossaryPartial from '@site/docs/partials/_glossary-partial.md';
-import {Quicklooks} from '@site/src/components/Quicklooks';
+import { Quicklooks } from '@site/src/components/Quicklooks';
 
 export default function FooterWrapper(props) {
   return (
