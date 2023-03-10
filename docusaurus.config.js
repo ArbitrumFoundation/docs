ee66194d3dadf6e61c2adc96f7a23818a7d70304
dzgoldman <dzgoldman@wesleyan.edu>
2023-03-10 15:12:19 -0500

    lint

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,7 +105,7 @@ const config = {
                 label: 'Telegram',
                 href: 'https://t.me/arbitrum',
               },
-            ]
+            ],
           },
           {
             title: 'More',
@@ -124,10 +124,10 @@ const config = {
         darkTheme: darkCodeTheme,
       },
       announcementBar: {
-        id: "support_us",
-         content: `Arbitrum DAO has been officially announced. The airdrop won’t be live until FINAL-TODO. Apply to be a delegate <a rel="noopener noreferrer" href="https://forum.arbitrum.foundation/t/delegation-submission-template/16">here</a>.`,
-        backgroundColor: "rgb(8 53 117)",
-        textColor: "white",
+        id: "banner",
+        content: `Arbitrum DAO has been officially announced. The airdrop won’t be live until FINAL-TODO. Apply to be a delegate <a rel="noopener noreferrer" href="https://forum.arbitrum.foundation/t/delegation-submission-template/16">here</a>.`,
+        backgroundColor: 'rgb(8 53 117)',
+        textColor: 'white',
         isCloseable: false,
       },
     }),
