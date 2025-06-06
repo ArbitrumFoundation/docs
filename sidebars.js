a19c11ee2539906617f9dd038b388f6ec9d894c2
Pete <petevielhaber@gmail.com>
2025-06-06 11:59:55 -0500

    added calculate aep fees doc from arb-docs; added new doc to sidebars

--- sidebars.js
+++ sidebars.js
@@ -87,6 +87,10 @@ module.exports = {
         {
           type: 'doc',
           id: 'how-tos/vote-dao-proposals',
+        },
+        {
+          type: 'doc',
+          id: 'how-tos/calculate-aep-fees',
         }
       ],
     },
